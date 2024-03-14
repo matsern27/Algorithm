f416807d6518a3661676fd5fcbb014e8a01207e9
matsern27 <mats.lindegaard@gmail.com>
2024-03-14 12:23:22 +0100

    InsetionSort, Boogo, merge

--- BobelSort.py
+++ BobelSort.py
@@ -1,12 +1,8 @@
 import pandas as pd
-
 df = pd.read_csv("Numbers")
 nums = [df["0"][n] for n in range(len(df))]
-
 for pointInd in range(len(nums)):
     for compareNumber in range(len(nums) - (1 + pointInd)):
         if nums[compareNumber] > nums[compareNumber + 1]:
             nums[compareNumber], nums[compareNumber + 1] = nums[compareNumber + 1], nums[compareNumber]
-
-
 print(nums)
